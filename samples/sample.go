44059dc8b7cff9e41c692c61fc6f21022b70853e
maru44 <takumaru22244@gmail.com>
2021-09-28 06:20:40 +0900

    sample

---
+++ samples/sample.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/maru44/perr"
+)
+
+func wrapSample() *perr.Err {
+	_, err := strconv.Atoi("sample")
+	return perr.Wrap(err, perr.FlagBadRequest)
+}
+
+func outputSample() *perr.Err {
+	_, err := strconv.Atoi("sample")
+	return perr.Wrap(err, perr.FlagBadRequest, "With Perr", "Have a nice day")
+}
+
+func main() {
+	fmt.Println("=================== Wrap error ===================")
+	sample1 := wrapSample()
+	fmt.Printf("For Output: %v\n", sample1.Output().Error())
+	fmt.Printf("For logging: %v\n", sample1.Error())
+	fmt.Printf("Stack trace:\n%v\n", sample1.Traces())
+
+	fmt.Println("=================== Wrap error ===================")
+	sample2 := outputSample()
+	fmt.Printf("For Output: %v\n", sample2.Output().Error())
+	fmt.Printf("For logging: %v\n", sample2.Error())
+	fmt.Printf("Dict:\n%v\n", sample2.ToDict())
+	fmt.Printf("01File: %v\n\n", sample2.ToDict().Traces[0].File)
+
+	fmt.Println("=================== New error ===================")
+	sample3 := perr.New("pouring coffee into tea cup", perr.FlagIAmTeaPot)
+	fmt.Printf("For Output: %v\n", sample3.Output().Error())
+	fmt.Printf("For logging: %v\n", sample3.Error())
+	fmt.Printf("Stack trace:\n%v\n", sample3.Traces())
+}
+
+/*                       output                       */
+
+// =================== Wrap error ===================
+// For Output: Bad Request
+// For logging: strconv.Atoi: parsing "sample": invalid syntax
+// Stack trace:
+// /home/secret/perr/samples/sample.go:33 ===> wrapSample
+// /home/secret/perr/samples/sample.go:12 ===> main
+
+// =================== Wrap error ===================
+// For Output: With Perr.Have a nice day
+// For logging: strconv.Atoi: parsing "sample": invalid syntax
+// Dict:
+// &{strconv.Atoi: parsing "sample": invalid syntax With Perr.Have a nice day EXTERNAL ERROR /home/secret/codes/perr/samples/sample.go:38 ===> outputSample
+// /home/secret/codes/perr/samples/sample.go:18 ===> main
+//  2021-09-28 06:18:05.5024428 +0900 JST m=+0.000136901}
+// 01File: /home/secret/codes/perr/samples/sample.go
+
+// =================== New error ===================
+// For Output: I'm a teapot
+// For logging: pouring coffee into tea cup
+// Stack trace:
+// /home/secret/codes/perr/samples/sample.go:25 ===> main
